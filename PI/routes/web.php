e4ae319bc5561d79f96836436215960e57cc5b70
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2022-03-30 20:21:34 -0300

    bug fix

--- PI/routes/web.php
+++ PI/routes/web.php
@@ -19,6 +19,13 @@ use App\Http\Controllers\TagController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
 Route::middleware(['auth'])->group(function(){
 
 
